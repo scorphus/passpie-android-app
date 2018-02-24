b9816bae15a2416d9fe085f923790d2f838a52a2
czhu99 <cthez72@gmail.com>
2018-02-24 11:25:23 -0600

    Added default project files

---
+++ app/src/main/java/com/example/z00c1/passpie_android_app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.z00c1.passpie_android_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
